d9ccf0282d87e28cda86dbe4905e73fb0f8c9b56
Aandeel <65628869+aandeel@users.noreply.github.com>
2023-08-02 17:14:57 +0100

    div

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,8 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        <div>
+          <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
@@ -27,6 +28,7 @@ function HomepageHeader() {
             to="https://discord.gg/6GeGaGjs97">
             Join the discord.
           </Link>
+        </div>
         </div>
       </div>
     </header>
