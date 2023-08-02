18bf70a2641b9c7bcfc4bb62fadae804814ae2c8
Aandeel <65628869+aandeel@users.noreply.github.com>
2023-08-02 17:03:29 +0100

    Commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'MCFunctions Docs',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Building an easy way to creating datapacks for Minecraft Java',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
